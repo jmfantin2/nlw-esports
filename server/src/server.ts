8dc6c676ccb06982dec644e34a5833fcfd05c88e
JM Fantin <jmfantin@outlook.com>
2022-10-13 14:04:31 -0300

    setup, web + mobile

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,15 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
+const app = express();
 
-app.get('/ads', (request, response) => {
+app.get("/ads", (request, response) => {
   return response.json([
-    {id:1,name:'ad1'},
-    {id:2,name:'ad2'},
-    {id:3,name:'ad3'},
-  ])
-})
+    { id: 1, name: "ad1" },
+    { id: 2, name: "ad2" },
+    { id: 3, name: "ad3" },
+    { id: 4, name: "ad4" },
+    { id: 5, name: "ad5" },
+  ]);
+});
 
-app.listen(3333)
+app.listen(3333);
---
+++ web/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>ae</h1>;
+}
+
+export default App;
