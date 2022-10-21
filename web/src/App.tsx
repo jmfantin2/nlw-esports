97ffdc5156cf2c0bddfac18e680a74ae69bf6784
JM Fantin <jmfantin@outlook.com>
2022-10-21 00:25:18 -0300

    web setup

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,39 @@
+import "./styles/main.css";
+import logoImg from "./assets/logo.svg";
+
 function App() {
-  return <h1>ae</h1>;
+  return (
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logoImg} alt="" />
+      <h1 className="text-6xl text-white font-black mt-20">
+        Tira as{" "}
+        <span className="bg-nlw-gradient bg-clip-text text-transparent">
+          carça
+        </span>
+        , tira as carça.
+      </h1>
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        <a href="">
+          <img src="/game-1.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/game-2.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/game-3.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/game-4.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/game-5.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/game-6.png" alt="" />
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ web/tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx',
+    './index.html'
+  ],
+  theme: {
+    extend: {
+      colors:{
+      },
+      backgroundImage: {
+        galaxy: 'url("/background-galaxy.png")',
+        'nlw-gradient': 'linear-gradient(90deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 44.57%)'
+      }
+    },
+  },
+  plugins: [],
+}
